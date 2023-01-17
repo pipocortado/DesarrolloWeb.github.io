7bdef13c019f77fb70cb096532200f89594c8207
Lucas Villafañez <lukitas_44556@hotmail.com>
2023-01-16 23:01:50 -0300

    Cambios de visualizacion de la pagina.

--- app.js
+++ app.js
@@ -5,3 +5,35 @@ let menu = document.getElementById("menu");
 menuToggle.addEventListener('click', e=> {
     menu.classList.toggle("show");
 })
+
+//Funciones carrusel
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("fotos-adro");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000);    
+}
+
+var index = 0;
+carrusel();
+
+function carrusel() {
+  var e;
+  var f = document.getElementsByClassName("fotos-uba");
+  for (e = 0; e < f.length; e++) {
+    f[e].style.display = "none";  
+  }
+  index++;
+  if (index > f.length) {index = 1}    
+  f[index-1].style.display = "block";  
+  setTimeout(carrusel, 4000);    
+}
