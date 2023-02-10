16174c606c1a229b646b300f4c6f4ef25656ff74
Lucas Villafañez <lukitas_44556@hotmail.com>
2023-02-09 21:16:34 -0300

    Cambios de estilos, logo y funcion modal.

--- app.js
+++ app.js
@@ -135,3 +135,75 @@ btnDerW.addEventListener('click' , function() {
 btnIzqW.addEventListener('click' , function() {
   AnteriorW();
 });
+
+//Funcion Modal.
+
+const openModal = document.querySelector("#img1");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector("#x");
+
+openModal.addEventListener("click" , (e)=> {
+    e.preventDefault();
+    modal.classList.add("modal--show");
+});
+
+closeModal.addEventListener("click" , (e)=> {
+    e.preventDefault();
+    modal.classList.remove("modal--show");
+});
+
+const openModal2 = document.querySelector("#img2");
+const modal2 = document.querySelector(".modal2");
+const closeModal2 = document.querySelector("#x2");
+
+openModal2.addEventListener("click" , (e)=> {
+    e.preventDefault();
+    modal2.classList.add("modal--show");
+});
+
+closeModal2.addEventListener("click" , (e)=> {
+    e.preventDefault();
+    modal2.classList.remove("modal--show");
+});
+
+const openModal3 = document.querySelector("#img3");
+const modal3 = document.querySelector(".modal3");
+const closeModal3 = document.querySelector("#x3");
+
+openModal3.addEventListener("click" , (e)=> {
+    e.preventDefault();
+    modal3.classList.add("modal--show");
+});
+
+closeModal3.addEventListener("click" , (e)=> {
+    e.preventDefault();
+    modal3.classList.remove("modal--show");
+});
+
+
+//    codigo del perfil aleatorio
+fetch("https://randomuser.me/api/")
+    .then(res => res.json())
+    .then(datos => {
+        let author = datos.results;
+        let datosPersonales = "<h2>Datos personales</h2>";
+        author.map(function (datos) {
+          datosPersonales += `
+            <div class="d-p">
+                <ul>
+                    <li><center><img src="${datos.picture.large}"></center></li>
+                    <li><h3>Nombre: ${datos.name.first}</h3></li>
+                    <li><h3>Apellido: ${datos.name.last}</h3></li>
+                    <li><h3>Numero: ${datos.cell}</h3></li>
+                    <li><h3>Fecha de nacimiento: ${datos.dob.date}</h3></li>
+                    <li><h3>Edad: ${datos.dob.age}</h3></li>
+                    <li><h3>Correo: ${datos.email}</h3></li>
+                    <li><h3>Ciudad: ${datos.location.city}</h3></li>
+                    <li><h3>Lugar de nacimiento: ${datos.location.country}</h3></li>
+                    <li><h3>Codigo postal: ${datos.location.postcode}</h3></li>
+                </ul>
+            </div> `;
+        });
+        document.getElementById('datos-personales').innerHTML = datosPersonales;
+
+    });
